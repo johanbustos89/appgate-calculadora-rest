e6a1c49f9586ccab5f3b01dda8fbe51937877d3c
CLAROCO\EUO5188A <Johan Bustos>
2022-04-05 18:42:07 -0500

    inicio git - proyecto appgate calculadora rest

---
+++ src/main/java/com/appgate/appgatecalculadorarest/AppgateCalculadoraRestApplication.java
@@ -0,0 +1,13 @@
+package com.appgate.appgatecalculadorarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppgateCalculadoraRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppgateCalculadoraRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appgate/appgatecalculadorarest/AppgateCalculadoraRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appgate.appgatecalculadorarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppgateCalculadoraRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
